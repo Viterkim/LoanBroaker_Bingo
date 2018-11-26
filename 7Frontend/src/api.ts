14d1a938f3f3eb07f9da934016f3295df75097ca
ExoMemphiz <ExoMemphiz@gmail.com>
2018-11-26 17:12:26 +0100

    Removed localhost testing

--- 7Frontend/src/api.ts
+++ 7Frontend/src/api.ts
@@ -1,5 +1,7 @@
 import WebSocketClient from "./nativeSocket";
 import { BankQuote } from "./types/BankTypes";
+import fetch, { RequestInit } from 'node-fetch';
+import { LoanObject } from "./types/CreditTypes";
 
 export function handleRequest(ssn: string, loanDuration: number, loanAmount: number, updateCallback: (bankQuote: BankQuote) => void, closeCallback: (code: number, reason: string) => void) {
     sendRequestToBanks(ssn, loanDuration, loanAmount);
@@ -10,6 +12,24 @@ export function handleRequest(ssn: string, loanDuration: number, loanAmount: num
 function sendRequestToBanks(ssn: string, loanDuration: number, loanAmount: number) {
     //TODO: POST request, with the ssn, duration and amount in body, and change the 1Get... project, 
     //so that it accepts this request and can handle all the data from there.
+    const SERVER_URL = "dolphin.viter.dk:9001";
+
+    const loanObject: LoanObject = {
+        ssn: ssn,
+        loanAmount: loanAmount,
+        loanDuration: loanDuration
+    }
+
+    const options: RequestInit = {
+        method: "POST",
+        body: JSON.stringify(loanObject)
+    }
+
+    fetch(SERVER_URL, options).then((result) => {
+        console.log(result);
+    }).catch((err) => {
+        console.log(err);
+    })
 }
 
 export function openWebsocket(ssn: string, updateCallback: (bankQuote: BankQuote) => void, closeCallback: (code: number, reason: string) => void) {
